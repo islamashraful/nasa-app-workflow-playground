84e3be3b1ca1f304567ca9695891f6c5b5f6d26e
Ashraful Islam <rx.ju.edu@gmail.com>
2024-07-02 21:18:25 +0700

    Check PR workflow

--- server/src/server.js
+++ server/src/server.js
@@ -6,9 +6,9 @@ const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
 const { loadLaunchData } = require("./models/launches.model");
 
-const server = http.createServer(app);
-
 const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
 
 (async function () {
   await connectMongo();
